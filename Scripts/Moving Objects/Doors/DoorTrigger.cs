cffae55bba90802ad29471740f95a9912e630bc1
Caden Kesey <33431966+cadenkesey@users.noreply.github.com>
2021-01-03 16:08:38 -0800

    Add files via upload

---
+++ Scripts/Moving Objects/Doors/DoorTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    private Transform _parentDoor;
+
+    private void Awake()
+    {
+        _parentDoor = this.transform.parent;
+    }
+
+    /// <summary>
+    /// Door needs to open if closing on someone
+    /// </summary>
+    /// <param name="other">The object colliding with the door trigger</param>
+    private void OnTriggerStay(Collider other)
+    {
+        _parentDoor.GetComponent<Door>().Bumped();
+    }
+
+    /// <summary>
+    /// A player is opening or closing the door
+    /// </summary>
+    public void Use()
+    {
+        _parentDoor.GetComponent<Door>().DoorTrigger();
+    }
+}
