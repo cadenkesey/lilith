f0da938179fe4129afabd216a5aefaa37e6e61b6
Caden Kesey <33431966+cadenkesey@users.noreply.github.com>
2021-01-03 16:48:18 -0800

    Add files via upload

---
+++ Scripts/Turret/TurretHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretHealth : MonoBehaviour
+{
+    /*[SerializeField]
+    private float health = 100f;
+    [SerializeField]
+    private float defense = 20f;*/
+    [SerializeField]
+    private GameObject _deadObject = default;
+
+    /// <summary>
+    /// Destroy the turret and replace it with a new object
+    /// </summary>
+    public void Die()
+    {
+        Instantiate(_deadObject, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
