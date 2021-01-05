db2be2260bd03f2660e4b8b7f4a8530d45bd8c2f
Caden Kesey <33431966+cadenkesey@users.noreply.github.com>
2021-01-04 20:03:16 -0800

    Add files via upload

---
+++ Scripts/BillboardShadow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardShadow : MonoBehaviour
+{
+    public GameObject _parentCharacter;
+
+    public string _filepath;
+
+    SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        Sprite current = Resources.Load<Sprite>(_filepath + _parentCharacter.GetComponent<SpriteRenderer>().sprite.name);
+        _spriteRenderer.sprite = current;
+    }
+
+    private void LateUpdate()
+    {
+        transform.forward = Camera.main.transform.forward;
+    }
+}
